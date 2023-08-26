60aa767f3e7ad39ef7dc0238adcd365437964e31
rofiadhimPS <muhammadrofiadhim@gmail.com>
2023-08-27 06:16:11 +0700

    refine documentation of sass

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -94,6 +94,8 @@ function toggleComplete(index) {
 
 <!-- style default bersifat global -->
 <!-- scoped untuk melimitasi hanya di komponen -->
+<!-- tambahkan lang="scss" agar bisa menggunakan fitur2 scss -->
+<!-- pastikan sudah install package 'sass'; 'npm i sass' -->
 <style scoped lang="scss">
 /* body = font-size: 16px (1rem) */
 
